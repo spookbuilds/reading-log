159182aa209e080d7700965cea7777d108c2e40d
spookbuilds <rosiesouter1993@gmail.com>
2026-07-31 20:13:11 +0100

    Can now add books to the list with the submit btn

--- script.js
+++ script.js
@@ -5,5 +5,24 @@ form.addEventListener("submit", function (event) {
   event.preventDefault();
 
   const data = new FormData(event.target);
-  console.log(data.get("title"));
+  const newEntry = {
+    title: data.get("title"),
+    author: data.get("author"),
+    type: data.get("type"),
+    status: data.get("status"),
+  };
+
+  console.log(newEntry);
+
+  const list = document.querySelector("#book-list");
+
+const article = document.createElement("article");
+article.className = "entry";
+article.innerHTML = `
+  <h2>${newEntry.title}</h2>
+  <p>${newEntry.author}</p>
+  <p>Status: ${newEntry.status}</p>
+`;
+
+list.appendChild(article);
 });
